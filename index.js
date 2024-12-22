4e9958a1c96a712464d2afb8fb373211980f8974
code-zeshan <zeshan.code@gmail.com>
2024-12-22 21:33:05 +0530

    Mail Server

---
+++ index.js
@@ -0,0 +1,30 @@
+    import {SMTPServer} from "smtp-server"
+
+    const server = new SMTPServer({
+        allowInsecureAuth: true,
+        authOptional:true,
+
+        onConnect(session,cb){
+            console.log(`onConnect`,session.id);
+            cb() // Accept the connection
+        },
+        onMailFrom(address, session, cb){
+            console.log(`onMailFrom`, address.address, session.id);
+            cb(); 
+        },
+        onRcptTo(address,session, cb){
+            console.log(`onRcptTo`, address.address, session.id);
+            cb();
+        },
+        onData(stream, session, cb){
+            stream.on('data', (data)=>{
+                console.log(`onData ${data.toString()}`);
+            stream.on('end',cb)
+            })
+        }
+
+    })
+
+    server.listen(5000,()=>{
+        console.log(`Server Running on 5000!`);
+    });
